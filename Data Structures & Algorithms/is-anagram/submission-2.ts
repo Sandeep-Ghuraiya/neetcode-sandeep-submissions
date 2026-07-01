e29df21d94d2eba05d55ddccef144a3b44712b1d
Sandeep-Ghuraiya <83284915+Sandeep-Ghuraiya@users.noreply.github.com>
2026-07-01 16:40:38 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        return s.split("").sort().join("") ===
+            t.split("").sort().join("");
+    }
+}
+
+const sol = new Solution();
+
+console.log(sol.isAnagram("racecar", "carrace"));
+console.log(sol.isAnagram("jar", "jam"))
