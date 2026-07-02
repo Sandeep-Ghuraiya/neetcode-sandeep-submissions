900fdce053b2cd8e578c1d784ba68406f5c05455
Sandeep-Ghuraiya <83284915+Sandeep-Ghuraiya@users.noreply.github.com>
2026-07-02 11:49:43 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const hashMap = new Map();
+
+        strs.forEach((str) => {
+            const key = str.split("").sort().join();
+
+            if(!hashMap.has(key)){
+                hashMap.set(key, []);
+            }
+
+            hashMap.get(key).push(str);
+        })
+
+        return [...hashMap.values()];
+    }
+}
+
+
+const sol = new Solution();
+
+console.log(sol.groupAnagrams(["act", "pots", "tops", "cat", "stop", "hat"])); // true
+console.log(sol.groupAnagrams(["x"])); // false
+console.log(sol.groupAnagrams([""]));
